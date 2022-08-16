26f8022d734e2b2d7e3887a5a9021fffcc499196
Ejim Michael <ejimmichael1@gmail.com>
2022-08-16 15:25:43 +0100

    Create useReducer hook

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,3 +1,33 @@
 import { createContext, useReducer } from 'react'; 
 
-export const GlobalContext = createContext();
+const initialTransac = {
+    transactions : [
+        { id: 1, text: 'Books', amount: -50 },
+        { id: 2, text: 'Phones', amount: -700 },
+        { id: 2, text: 'Salary', amount: 2000 },
+        { id: 3, text : 'Incomes', amount: 100 },
+    ]
+}
+
+export const GlobalContext = createContext(initialTransac);
+
+const AppReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialTransac);
+
+    return ( 
+        <GlobalContext.Provider value={{
+            transactions: state.transactions
+        }}>
+            {children}
+        </GlobalContext.Provider>
+     );
+}
+ 
+export default GlobalProvider;
