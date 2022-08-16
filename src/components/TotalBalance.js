1c56936fab1b2786c41a871212f4b7dfe2f7a8eb
Ejim Michael <ejimmichael1@gmail.com>
2022-08-16 22:20:02 +0100

    complete totalbalance functionality

--- src/components/TotalBalance.js
+++ src/components/TotalBalance.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import { useContext } from 'react';
+import { GlobalContext } from "../context/GlobalContext";
+
 
 function TotalBalance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, curr) => (acc += curr), 0).toFixed(2);
+
   return (
     <div className="mt-6 text-left">
       <h1 className="text-3xl font-medium text-gray-600">
         Total Balance
       </h1>
-      <h4 className='font-medium text-xl'>$0.00</h4>
+      <h4 className='font-medium text-xl'>${total}</h4>
     </div>
   );
 }
 
-export default TotalBalance
+export default TotalBalance;
