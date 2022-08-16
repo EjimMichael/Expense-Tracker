19b1b2b657dc4b5ffaedaa321b91990260bd54fb
Ejim Michael <ejimmichael1@gmail.com>
2022-08-16 21:27:31 +0100

    Add delete button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,18 +4,23 @@ function Transaction({ transaction }) {
     const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li className="flex justify-between border pl-2 bg-white font-medium text-gray-700">
-      {transaction.text}
-      <p
-        className={
-          transaction.amount < 0
-            ? "border-r-4 pr-2 py border-r-red-600"
-            : "border-r-4 pr-2 py border-r-green-600"
-        }
-      >
-        {sign}${Math.abs(transaction.amount)}
-      </p>
-    </li>
+    <div className="flex group">
+      <li className="w-full flex justify-between border pl-2 bg-white font-medium text-gray-700">
+        {transaction.text}
+        <p
+          className={
+            transaction.amount < 0
+              ? "border-r-4 pr-2 py border-r-red-600"
+              : "border-r-4 pr-2 py border-r-green-600"
+          }
+        >
+          {sign}${Math.abs(transaction.amount)}
+        </p>
+      </li>
+      <button className="hidden group-hover:block text-red-700 font-medium pl-1 text-2xl">
+        x
+      </button>
+    </div>
   ); 
 }
 
